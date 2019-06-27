9d8d3bb5257bd2730b29ceb6ddcaec861f16157e
ghun <ghun2ee@gmail.com>
2019-06-27 15:18:32 +0900

    [test] flask-restful
    use flask-restful library

--- app.py
+++ app.py
@@ -1,12 +1,58 @@
 from flask import Flask
+from flask_restful import Resource, Api, reqparse, abort
 
 app = Flask(__name__)
+api = Api(app)
 
+TODOS = {
+    'todo1':{'task':'build an API'},
+    'todo2':{'task':'????'},
+    'todo3':{'task':'profit!'},
+}
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+def abort_if_todo_doesnt_exist(todo_id):
+    if todo_id not in TODOS:
+        abort(404,message="Todo {} doesn't exist".format(todo_id))
 
+parser = reqparse.RequestParser()
+parser.add_argument('task')
+
+# To_do
+# shows a single to_do item and lets you delete a to_do item
+class Todo(Resource):
+    def get(self,todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        return TODOS[todo_id]
+
+    def delete(self,todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        del TODOS[todo_id]
+        return '', 204
+
+    def put(self,todo_id):
+        args = parser.parse_args()
+        task = {'task': args['task']}
+        TODOS[todo_id] = task
+        return task,201
+
+# TodoList
+# shows a list of all todos, and lets you post to add new tasks
+class TodoList(Resource):
+    def get(self):
+        return TODOS
+
+    def post(self):
+        args = parser.parse_args()
+        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
+        todo_id = 'todo%i' % todo_id
+        TODOS[todo_id] = {'task': args['task']}
+        return TODOS[todo_id], 201
+
+##
+## Actually setup the Api resource routing here
+##
+api.add_resource(TodoList,'/todos')
+api.add_resource(Todo,'/todos/<todo_id>')
 
 if __name__ == '__main__':
     app.run()
